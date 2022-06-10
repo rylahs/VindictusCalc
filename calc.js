3be56a273664894a9231fbc421d92bacc8e630d1
Rylah <94848571+rylahs@users.noreply.github.com>
2022-06-09 21:22:29 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,392 @@
+const bossstat = {
+    claire: {
+        def: 21500,
+        res: 113,
+        bosscounterforce: 5,
+        bosscounterforcemindmg: 30,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    elchulus: {
+        def: 22000,
+        res: 114,
+        bosscounterforce: 10,
+        bosscounterforcemindmg: 20,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    macha: {
+        def: 22500,
+        res: 116,
+        bosscounterforce: 20,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    agares: {
+        def: 25000,
+        res: 120,
+        bosscounterforce: 30,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    lugh: {
+        def: 26000,
+        res: 125,
+        bosscounterforce: 35,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    selren: {
+        def: 26500,
+        res: 130,
+        bosscounterforce: 40,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    marject: {
+        def: 27000,
+        res: 136,
+        bosscounterforce: 45,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    aodhan: {
+        def: 27600,
+        res: 142,
+        bosscounterforce: 50,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    caesar: {
+        def: 29000,
+        res: 143,
+        bosscounterforce: 55,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    nyle: {
+        def: 30100,
+        res: 148,
+        bosscounterforce: 60,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 1,
+        bossalrlimit: 5800
+    },
+    ragnahim: {
+        def: 31200,
+        res: 154,
+        bosscounterforce: 65,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 2,
+        bossalrlimit: 5800
+    },
+    siete: {
+        def: 32300,
+        res: 160,
+        bosscounterforce: 70,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 3,
+        bossalrlimit: 5800
+    },
+    epona: {
+        def: 32300,
+        res: 167,
+        bosscounterforce: 75,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 4,
+        bossalrlimit: 5800
+    },
+    tarathan: {
+        def: 32300,
+        res: 174,
+        bosscounterforce: 80,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 4,
+        bossalrlimit: 5800
+    },
+    romel: {
+        def: 32300,
+        res: 175,
+        bosscounterforce: 88,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 7,
+        bossalrlimit: 8000
+    },
+    naberius: {
+        def: 34110,
+        res: 181,
+        bosscounterforce: 92,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 9,
+        bossalrlimit: 8000
+    },
+    milo: {
+        def: 35920,
+        res: 188,
+        bosscounterforce: 100,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 12,
+        bossalrlimit: 8000
+    },
+    ceasair: {
+        def: 37780,
+        res: 195,
+        bosscounterforce: 108,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 14,
+        bossalrlimit: 8000
+    },
+    special: {
+        def: 32300,
+        res: 175,
+        bosscounterforce: 88,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 7,
+        bossalrlimit: 8000
+    },
+    neamhain: {
+        def: 17000,
+        res: 90,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    balor: {
+        def: 23500,
+        res: 123,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    brigit: {
+        def: 30000,
+        res: 150,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    laura: {
+        def: 35500,
+        res: 185,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 4,
+        bossalrlimit: 5800
+    },
+    hell1: {
+        def: 29000,
+        res: 145,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    hell2: {
+        def: 35500,
+        res: 185,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 0,
+        bossalrlimit: 5800
+    },
+    hell3: {
+        def: 37280,
+        res: 206,
+        bosscounterforce: 0,
+        bosscounterforcemindmg: 100,
+        bossbalforce: 17,
+        bossalrlimit: 8000
+    },
+    taros: {
+        def: 41000,
+        res: 229,
+        bosscounterforce: 180,
+        bosscounterforcemindmg: 10,
+        bossbalforce: 34,
+        bossalrlimit: 9500
+    }
+}
+
+function exec(num) {
+    target = event.target.parentElement.parentElement;
+
+    let boss;
+    if (document.querySelector('select#boss').value == 'custom')
+        boss = {
+            def: document.querySelector('input#bossdef').value * 1,
+            res: document.querySelector('input#bossres').value * 1,
+            bosscounterforce: document.querySelector('input#bosscounterforce').value * 1,
+            bosscounterforcemindmg: document.querySelector('input#bosscounterforcemindmg').value * 1,
+            bossbalforce: document.querySelector('input#bossbalforce').value * 1
+        };
+    else
+        boss = bossstat[document.querySelector('select#boss').value];
+    let atk = target.querySelector('input#atk').value * 1;
+    let add = target.querySelector('input#add').value * 1;
+    let alr = target.querySelector('input#alr').value * 1;
+    let bal = target.querySelector('input#bal').value * 1 - boss.bossbalforce * 1;
+
+    if (bal > 100)
+        bal = 100;
+
+    if (alr > boss.bossalrlimit)
+        alr = boss.bossalrlimit;
+    
+    let cri = target.querySelector('input#cri').value * 1;
+    let counterforce = target.querySelector('input#counterforce').value * 1;
+    let swordl;
+    let spearl;
+    if (num == 1) {
+        swordl = document.querySelector('#swordl1').checked;
+        spearl = document.querySelector('#spearl1').checked;
+    } else if (num == 2) {
+        swordl = document.querySelector('#swordl2').checked;
+        spearl = document.querySelector('#spearl2').checked;
+    }
+
+    dmg = calcdmg(boss, atk, add, alr, bal, cri, counterforce, swordl, spearl);
+
+
+    target.querySelector('input#nocritdmg').value = Math.round(dmg[0] * 100) / 100;
+    target.querySelector('input#critdmg').value = Math.round(dmg[1] * 100) / 100;
+}
+
+function calcdmg(boss, atk, add, alr, bal, cri, counterforce, swordl, spearl) {
+    let att = Math.max(Math.min(10000 + alr, atk - boss.def), 0);
+    let base;
+    if (att <= (boss.def * 2)) {
+        let x = (att + 900) / (boss.def + 900);
+        let fx = 0.1856 + 0.5525 * x + 0.4214 * Math.pow(x, 2) - 0.3094 * Math.pow(x, 3) + 0.3643 * Math.pow(x, 4) - 0.2144 * Math.pow(x, 5);
+        base = (boss.def + 900) * fx;
+    } else {
+        base = (boss.def + 900) + (att - 2 * boss.def);
+    }
+
+    let adm;
+    if (att <= 3000)
+        adm = 1.875;
+    else if (att <= 10000)
+        adm = 6.25 * att / 10000;
+    else
+        adm = 6.25 + (att - 10000) / 2400;
+
+    let effcrit;
+    if (swordl) effcrit = Math.max(Math.min(65, cri + 15 - boss.res), 3);
+    else if (spearl) effcrit = Math.max(Math.min(65, cri + 22 - boss.res), 3);
+    else effcrit = Math.max(Math.min(50, cri - boss.res), 3);
+
+    let nocritdmg = (base + add * adm) * (bal + 100) / 200;
+    if (boss.bosscounterforce > 0) {
+        let ddm = Math.max(Math.min(counterforce - boss.bosscounterforce, 100), boss.bosscounterforcemindmg) / 100;
+        nocritdmg = nocritdmg * ddm;
+    }
+
+    let critdmg;
+    if (swordl) critdmg = nocritdmg * (2.25 * (effcrit / 100) + 1 * ((100 - effcrit) / 100));
+    else critdmg = nocritdmg * (1.95 * (effcrit / 100) + 1 * ((100 - effcrit) / 100));
+
+    return [nocritdmg, critdmg];
+}
+
+function compare() {
+    let comptype;
+    if (document.querySelector('#compcrit').checked) comptype = 'crit';
+    else if (document.querySelector('#compnocrit').checked) comptype = 'nocrit';
+
+    let dmg1, dmg2;
+    if (comptype == 'crit') {
+        dmg1 = document.querySelector('#calc1 input#critdmg').value * 1;
+        dmg2 = document.querySelector('#calc2 input#critdmg').value * 1;
+    } else if (comptype == 'nocrit') {
+        dmg1 = document.querySelector('#calc1 input#nocritdmg').value * 1;
+        dmg2 = document.querySelector('#calc2 input#nocritdmg').value * 1;
+    }
+
+    let inputdeal1 = document.querySelector('#compare #input input#deal1').value * 1;
+    let inputdeal2 = document.querySelector('#compare #input input#deal2').value * 1;
+
+    let samespecdeal1 = inputdeal1 * 100000 / dmg1;
+    let samespecdeal2 = inputdeal2 * 100000 / dmg2;
+
+    let outputdeal1 = samespecdeal1 / (samespecdeal1 + samespecdeal2) * 100;
+    let outputdeal2 = samespecdeal2 / (samespecdeal1 + samespecdeal2) * 100;
+
+    document.querySelector('#compare #output input#deal1').value = Math.round(outputdeal1 * 100) / 100;
+    document.querySelector('#compare #output input#deal2').value = Math.round(outputdeal2 * 100) / 100;
+}
+
+function timecalc(num) {
+    let comptype;
+    if (document.querySelector('#compcrit').checked) comptype = 'crit';
+    else if (document.querySelector('#compnocrit').checked) comptype = 'nocrit';
+
+    let boss;
+    let swordl, spearl;
+    if (document.querySelector('select#boss').value == 'custom')
+        boss = {
+            def: document.querySelector('input#bossdef').value * 1,
+            res: document.querySelector('input#bossres').value * 1,
+            bosscounterforce: document.querySelector('input#bosscounterforce').value * 1,
+            bosscounterforcemindmg: document.querySelector('input#bosscounterforcemindmg').value * 1
+        };
+    else
+        boss = bossstat[document.querySelector('select#boss').value];
+    if (num == 1) {
+        swordl = document.querySelector('#swordl1').checked;
+        spearl = document.querySelector('#spearl1').checked;
+    } else if (num == 2) {
+        swordl = document.querySelector('#swordl2').checked;
+        spearl = document.querySelector('#spearl2').checked;
+    }
+
+    let dmg, fullspecdmg, inputtime, fullspectime, timedeal;
+
+    if (num == 1) {
+        if (comptype == 'crit') {
+            dmg = document.querySelector('#calc1 input#critdmg').value * 1;
+            fullspecdmg = calcdmg(boss, 99999, 7000, 7500, 150, 999, 999, swordl, spearl)[1];
+        } else if (comptype == 'nocrit') {
+            dmg = document.querySelector('#calc1 input#nocritdmg').value * 1;
+            fullspecdmg = calcdmg(boss, 99999, 7000, 7500, 150, 999, 999, swordl, spearl)[0];
+        }
+        inputtime = document.querySelector('#timecalc #input input#min1').value * 1 + document.querySelector('#timecalc #input input#sec1').value * 1 / 60;
+        timedeal = document.querySelector('#timecalc #input input#timedeal1').value * 1;
+    } else if (num == 2) {
+        if (comptype == 'crit') {
+            dmg = document.querySelector('#calc2 input#critdmg').value * 1;
+            // (boss, atk, add, alr, bal, cri, counterforce, swordl, spearl)
+            fullspecdmg = calcdmg(boss, 99999, 7000, 7500, 150, 999, 999, swordl, spearl)[1];
+        } else if (comptype == 'nocrit') {
+            dmg = document.querySelector('#calc2 input#nocritdmg').value * 1;
+            fullspecdmg = calcdmg(boss, 99999, 7000, 7500, 150, 999, 999, swordl, spearl)[0];
+        }
+        inputtime = document.querySelector('#timecalc #input input#min2').value * 1 + document.querySelector('#timecalc #input input#sec2').value * 1 / 60;
+        timedeal = document.querySelector('#timecalc #input input#timedeal2').value * 1;
+    }
+
+    fullspectime = inputtime * dmg / fullspecdmg * 100 / timedeal;
+
+    fullspecmin = Math.floor(fullspectime);
+    fullspecsec = Math.round((fullspectime - fullspecmin) * 60);
+    fullspecdpm = Math.round((100 / fullspectime * 100)) / 100;
+
+    if (num == 1) {
+        document.querySelector('#timecalc #output input#min1').value = fullspecmin;
+        document.querySelector('#timecalc #output input#sec1').value = fullspecsec;
+        document.querySelector('#timecalc #output input#dpm1').value = fullspecdpm;
+    } else if (num == 2) {
+        document.querySelector('#timecalc #output input#min2').value = fullspecmin;
+        document.querySelector('#timecalc #output input#sec2').value = fullspecsec;
+        document.querySelector('#timecalc #output input#dpm2').value = fullspecdpm;
+    }
+}
